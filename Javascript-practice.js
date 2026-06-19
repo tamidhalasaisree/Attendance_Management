7a1ee3ab3b2b45919aa0c639de612aeb2f759a9b
T,Sai Sree <saishreereddy21@gmail.com>
2026-06-19 14:21:45 +0530

    add javascript-practice.js

---
+++ Javascript-practice.js
@@ -0,0 +1,15 @@
+//let a=10
+ //a+=10
+//console.log(a)
+//console.log(--a)
+//console.log(a)
+const a=10
+const b=20
+const c=30
+if(a>b&&a>c){
+  console.log("a is greater")
+} else if(b>a&&b>c){
+  console.log("b is greater")
+} else {
+  console.log("c is greater")
+}
