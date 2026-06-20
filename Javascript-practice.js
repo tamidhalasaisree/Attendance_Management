3dfa5bad1c96662f5d95c3056422f50003b9fc99
T,Sai Sree <saishreereddy21@gmail.com>
2026-06-20 11:28:12 +0530

    add javascript.js

--- Javascript-practice.js
+++ Javascript-practice.js
@@ -1,50 +1,82 @@
-
-const age=16
-const licence=true
-if(age>=18){
-    if(licence)
-       console.log("You can drive")
-    } else if(age>=18&& !licence){
-        console.log("You need a valid license to drive")
-    }
-    else{
-      console.log("You are not old enough to drive")
-    }
-
-    
-  
-    const obj={
-      name:"Samsung A50",
-      disc:"Stlish Phone",
-      price:25000
-    }
-  for(let mobile in obj){
-    console.log(obj[mobile])
-  }
-  const arr2=[{
-      name:"Samsung A50",
-      disc:"Stylish Phone",
-      price:25000
-  },
-  {
-      name:"Google pixel",
-      disc:"Stylish Phone",
-      price:35000
-  },
-  {
-      name:"One Plus",
-      disc:"Stylish Phone",
-      price:25000
-  }
-];
- for(let mobile of arr2){
-    //console.log(mobile)
-    for(let details in mobile){
-      console.log(mobile[details])
-    }
-}   
-
-  
-  
+// const age=16
+// const licence=true
+// if(age>=18){
+//     if(licence)
+//        console.log("You can drive")
+//     } else if(age>=18&& !licence){
+//         console.log("You need a valid license to drive")
+//     }
+//     else{
+//       console.log("You are not old enough to drive")
+//     }
 
 
+
+//     const obj={
+//       name:"Samsung A50",
+//       disc:"Stlish Phone",
+//       price:25000
+//     }
+//   for(let mobile in obj){
+//     console.log(obj[mobile])
+//   }
+//   const arr2=[{
+//       name:"Samsung A50",
+//       disc:"Stylish Phone",
+//       price:25000
+//   },
+//   {
+//       name:"Google pixel",
+//       disc:"Stylish Phone",
+//       price:35000
+//   },
+//   {
+//       name:"One Plus",
+//       disc:"Stylish Phone",
+//       price:25000
+//   }
+// ];
+//  for(let mobile of arr2){
+//     //console.log(mobile)
+//     for(let details in mobile){
+//       console.log(mobile[details])
+//     }
+// }   
+// const arr=[10,5,500,602,45,[5,3,25]]
+// console.log(arr.push(6))
+// console.log(arr)
+// console.log(arr.pop())
+// console.log(arr)
+// console.log(arr.unshift("Arun"))
+// console.log(arr)
+// console.log(arr.shift())
+// console.log(arr)
+
+// const arr2=[10,"Vamshi",500,"Sathi babu"]
+// arr2.splice(2,1)
+// console.log(arr2)
+// arr2.splice(0,2,"karthik")
+// console.log(arr2)
+//  const arr3=["Arun","Vamshi","Anusha"]
+//  console.log(arr3.includes())
+
+//  const  studentreport = arr.find(
+//   s => s.name === "Teja"
+// )
+// console.log(studentreport)
+
+// arr.forEach(student =>{
+//   console.log(student)
+// })        //for each method is used to iterate each element from an array
+
+// const  modarr = arr.map(student =>console.log(student))
+// console.log(modarr)
+
+//  const arrx=[2,50,18]
+//  const modarrx = arrx.map(element => element + 5)
+//  console.log(modarrx)
+
+//  const filter_arr= arr.filter(student => student.Dept === "CSM")
+//  console.log(filter_arr)
+
+//  console.log(filter_arr.length)
